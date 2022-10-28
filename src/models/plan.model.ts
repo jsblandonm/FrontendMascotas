dc06e1ecbcafaa82813e7f8b4fb886ba63416297
Jsblandonm <sebasblandonmontoya@gmail.com>
2022-10-28 11:48:42 -0500

    cle

--- src/models/plan.model.ts
+++ src/models/plan.model.ts
@@ -1,4 +1,4 @@
-import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Entity, hasMany, model, property} from '@loopback/repository';
 import {Mascota} from './mascota.model';
 
 @model()
@@ -14,7 +14,7 @@ export class Plan extends Entity {
     type: 'string',
     required: true,
   })
-  Tipo_de_plan: string;
+  Tipodeplan: string;
 
   @property({
     type: 'date',
