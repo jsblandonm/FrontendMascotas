9e8c5a0b7a4cf301823e4699d072f4f2b7e2522a
Jsblandonm <sebasblandonmontoya@gmail.com>
2022-11-22 21:16:23 -0500

    Guardianes

--- Frontend_Veterinaria-angular/src/app/modules/administration/administration-routing.module.ts
+++ Frontend_Veterinaria-angular/src/app/modules/administration/administration-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ValidatorSesionGuard } from 'src/app/guard/validator-sesion.guard';
 import { CreateUserComponent } from './user/create-user/create-user.component';
 import { DeleteUserComponent } from './user/delete-user/delete-user.component';
 import { EditUserComponent } from './user/edit-user/edit-user.component';
@@ -9,23 +10,28 @@ import { SearchUserComponent } from './user/search-user/search-user.component';
 const routes: Routes = [
   {
     path : 'create-user',
-    component : CreateUserComponent
+    component : CreateUserComponent,
+    canActivate : [ValidatorSesionGuard]
   },
   {
     path: 'listUser',
-    component : SearchUserComponent
+    component : SearchUserComponent,
+    canActivate : [ValidatorSesionGuard]
   },  
   {
     path : 'edit-user/:id',
-    component : EditUserComponent
+    component : EditUserComponent,
+    canActivate : [ValidatorSesionGuard]
   },
   {
     path : 'delete-user',
-    component : DeleteUserComponent
+    component : DeleteUserComponent,
+    canActivate : [ValidatorSesionGuard]
   },
   {
     path : 'search-user',
-    component	 : SearchUserComponent
+    component	 : SearchUserComponent,
+    canActivate : [ValidatorSesionGuard]
   }
 ];
 
